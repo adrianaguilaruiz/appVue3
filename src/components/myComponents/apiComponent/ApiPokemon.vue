20a06545e1951de687958d780655daf2e8744d29
Adriana Aguilar <agruad@gmail.com>
2023-04-16 10:22:18 +0200

    connected pokemon api with axios

---
+++ src/components/myComponents/apiComponent/ApiPokemon.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios'
+
+    export default {
+        data() {
+            return {
+                pokemonData: null,
+
+            }
+        },
+        //lifecycle (option or composition api)
+        created() {
+            axios.get('https://pokeapi.co/api/v2/pokemon/umbreon').then((result) => {
+                this.pokemonData = result.data
+            })
+        }
+
+    }
+</script>
+
+
+<template>
+    <div>
+        <div v-if="pokemonData">
+            <h2>{{ pokemonData.name }}</h2>
+            <img class="imgPokemon" :src="pokemonData.sprites.other.dream_world.front_default" alt="Pokemon image">
+            <img class="imgPokemon" :src="pokemonData.sprites.other.home.front_default" alt="Pokemon image">
+            <img class="imgPokemon" :src="pokemonData.sprites.other.home.front_shiny" alt="Pokemon image">
+            
+            <h3>HP: {{ pokemonData.stats[0].base_stat }}</h3>
+            <h3>Attack: {{ pokemonData.stats[1].base_stat }}</h3>
+            <h3>Defense: {{ pokemonData.stats[2].base_stat }}</h3>
+            <h3>Speed: {{ pokemonData.stats[3].base_stat }}</h3>
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    .imgPokemon {
+        width: 200px;
+    }
+</style>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MyView from '../views/MyView.vue'
+import ApiPokemonView from '../views/ApiPokemonView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/myview',
       name: 'MyView',
       component: MyView
+    },
+    {
+      path: '/pokemon',
+      name: 'pokemon',
+      component: ApiPokemonView,
     }
   ]
 })
